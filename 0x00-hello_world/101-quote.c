5c000b9bbe5564eb24486fbeba0b5a7f2da99b2a
belal <belalismail252@gmail.com>
2023-10-05 13:27:52 -0700

    UNIX is basically a simple operating system task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: using write function to print a quote
+ *	write(int fd,const void *buf,size_t count)
+ *
+ *Return: 1 (not Success)
+*/
+int main(void)
+{
+char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(1, quo, 59);
+return (1);
+}
