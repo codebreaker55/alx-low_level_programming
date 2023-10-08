88c486cb81872a93972b4f4da141559620e20fff
belal <belalismail252@gmail.com>
2023-10-08 09:45:45 -0700

    I play what I call the alphabet game task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * Description: using putchar to print all alphabet letters in lowercase
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet >= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+
+	return (0);
+}
