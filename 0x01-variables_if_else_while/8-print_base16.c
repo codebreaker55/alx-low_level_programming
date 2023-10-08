525720312bcf016e9beff67bbb0137cbf535d3cf
belal <belalismail252@gmail.com>
2023-10-08 12:55:15 -0700

    Hexadecimal task

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: all the numbers of base 16 in lowercase followed by a new line
+ *
+ * Return: always 0 (Success)
+*/
+
+int main(void)
+{
+	int num = 48; /*48; decimal rep of 0*/
+
+	while (num <= 102) /*102; decimal rep of f*/
+	{
+		putchar(num);
+
+		/*after 9 we jump till 96;*/
+		if (num == 57)
+			num += 39;
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
