d0f8bffa74525362f5bae0546483491ee4b8ae4a
belal <belalismail252@gmail.com>
2023-10-08 17:24:38 -0700

    Software is eating the World task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: all possible combinations of two two-digit numbers
+ *
+ * Return: always 0 (Success)
+*/
+
+int main(void)
+{
+	int digit1 = 0, digit2;
+
+	while (digit1 <= 99)
+	{
+		digit2 = digit1;
+		while (digit2 <= 99)
+		{
+			if (digit2 != digit1)
+			{
+				putchar((digit1 / 10) + 48);
+				putchar((digit1 % 10) + 48);
+				putchar(' ');
+				putchar((digit2 / 10) + 48);
+				putchar((digit2 % 10) + 48);
+
+				if (digit1 != 98 || digit2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			digit2++;
+		}
+		digit1++;
+	}
+	putchar('\n');
+	return (0);
+}
