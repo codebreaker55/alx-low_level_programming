10fbe81cd49ea9ddca557bd6a5435f536f1ef359
belal <belalismail252@gmail.com>
2023-10-10 06:37:16 -0700

    _putchar task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: using _putchar prototype to print _putchar
+ *
+ * Return: always 0 (Success)
+*/
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
+	_puthar('\n');
+	return (0);
+}
