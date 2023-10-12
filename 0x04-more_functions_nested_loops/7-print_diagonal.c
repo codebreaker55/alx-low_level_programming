ebeefce800d19fb3f8ccac64c7693f3033ae4cae
belal <belalismail252@gmail.com>
2023-10-12 08:57:23 -0700

    in parallel universe task

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,7 +3,8 @@
 /**
  * print_diagonal - function that draws a diagonal line on the terminal
  *
- * @n: is the number of times the character \ should be printed
+ * @n: is the number of times the character \
+ *	should be printed
 */
 
 void print_diagonal(int n)
