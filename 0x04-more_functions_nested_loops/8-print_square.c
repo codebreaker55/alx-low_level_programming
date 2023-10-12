93ace511feb030e1fa5ac281fb235760f68c72ee
belal <belalismail252@gmail.com>
2023-10-12 09:28:31 -0700

    You are so much sunshine in every square inch task

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_square - function that prints a square, followed by a new line
+ *
+ * @size: is the size of the square
+ *
+ * Return: always 0 (Success)
+*/
+
+void print_square(int size)
+{
+	int row, column;
+
+	for (row = 1; row <= size; row++)
+	{
+		for (column = 1; column <= size; column++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
