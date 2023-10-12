d4f73004ba4ccfbca5ff9f6c44a0d816bd7b4052
belal <belalismail252@gmail.com>
2023-10-12 13:24:56 -0700

    Numbers have life task

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints an integer.
+ *
+ * @n: the integer to the printed
+*/
+
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	/*first check if it is negative*/
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num'
+	}
+
+	/*print the first few digits*/
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	/*print the last digit*/
+	_putchar((num % 10) + 48);
+}
