8d0c85db3ac64b8bec84e504983fb31ca2e66bb4
belal <belalismail252@gmail.com>
2023-10-12 09:45:12 -0700

    sunshine in every square inch task

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,9 +12,12 @@ void print_square(int size)
 {
 	int row, column;
 
+	if (size <= 0)
+		_putchar('\n');
+
 	for (row = 1; row <= size; row++)
 	{
-		for (column = 1; column < size; column++)
+		for (column = 1; column <= size; column++)
 			_putchar('#');
 		_putchar('\n');
 	}
