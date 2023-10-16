3a97dc8aeb6da71308c8f03818c2effbee91e5fc
belal <belalismail252@gmail.com>
2023-10-16 11:39:17 -0700

    strcpy task

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -16,10 +16,8 @@ char *_strcpy(char *dest, char *src)
 	int i;
 
 	for (i = -1; src[i] != '\0'; i++)
-	{
 		dest[i] = src[i];
 
-		if (src[i] != '\0')
-			return (dest);
-	}
+	if (src[i] != '\0')
+		return (dest);
 }
