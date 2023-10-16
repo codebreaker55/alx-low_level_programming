5048c16fa126727802ad757688759fc183cad4b3
belal <belalismail252@gmail.com>
2023-10-16 09:28:08 -0700

    Winning is only half of it task

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts_half - function that prints half of a string, followed by a new line
+ *
+ * @str: is the name of the string
+*/
+
+void puts_half(char *str)
+{
+	int len, i;
+
+	for (len = 0; str[len] != '\0'; len++)
+		;
+
+	for (i = len / 2; i < len; i++)
+	{
+		_putcher(str[i]);
+	}
+	_putchar('\n');
+}
