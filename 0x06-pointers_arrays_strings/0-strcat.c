5d57535dd4e9a6fd43fb8500e24c412181db2b68
belal <belalismail252@gmail.com>
2023-10-18 09:05:16 -0700

    the strcat task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,10 +14,12 @@ char *_strcat(char *dest, char *src)
 	int i, j;
 
 	for (i = 0; dest[i]; i++)
+		;
+
+	for (j = 0; src[j]; j++)
 	{
-		for (j = 0; src[j]; j++)
-			dest[i] = src[j];
-		i++
+		dest[i] = src[j];
+		i++;
 	}
 	return (dest);
 }
