52a46506398a1f3d57358b98ba1e2b7826aeded9
belal <belalismail252@gmail.com>
2023-10-18 13:13:15 -0700

    always look up task

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -13,9 +13,12 @@ char *string_toupper(char *str)
 {
 	int i;
 
-	for (i = 0; i >= 'a' && i <= 'z'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		str[i] = (str[i] - 32);
+		if (i >= 'a' && i <= 'z')
+		{
+			str[i] = (str[i] - 32);
+		}
 	}
 	return (str);
 }
