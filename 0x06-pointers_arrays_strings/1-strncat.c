048899a4e185c838a2ff01c31feebd3bb8e005cc
belal <belalismail252@gmail.com>
2023-10-18 09:12:58 -0700

    the strncat task

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -20,9 +20,8 @@ char *_strncat(char *dest, char *src, int n)
 	for (j = 0; j < n && src[j] != '\0'; j++)
 	{
 		dest[i + j] = src[j];
-		dest[i + j] = '\0';
 	}
+	dest[i + j] = '\0';
+
 	return (dest);
 }
-
-
