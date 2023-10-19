df2ea449fabe2beee6d299ce15c2e798b9a86e3f
belal <belalismail252@gmail.com>
2023-10-19 09:31:48 -0700

    rot13 task

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *rot13 - function that encodes a string using rot13
+ *
+ * @str: is the name of the string
+ *
+ * Return: a pointer to string after it get encode
+*/
+
+char *rot13(char *str)
+{
+	*ptr = str;
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i;
+
+	while (*str)
+	{
+		for (i = 0; i <= 52; i++)
+		{
+			if (*str == input[i])
+			{
+				*str = output[i];
+				break;
+			}
+		}
+		str++;
+	}
+	return (ptr);
+}
