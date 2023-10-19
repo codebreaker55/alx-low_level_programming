25d6e1cf0bc587bd1f52dfc6e31e579eaead853e
belal <belalismail252@gmail.com>
2023-10-19 04:29:34 -0700

    for everybody task

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *leet - Write a function that encodes a string into 1337
+ *
+ * @str: is the name of the string
+ *
+ * Return: a pointer to str string
+*/
+
+char *leet(char *str)
+{
+	char *ptr = str;
+	char ch[] = ('a', 'e', 'o', 't', 'l');
+	int num[] = ('4', '3', '0', '7', '1');
+	unsigned int i;
+
+	while (*str)
+	{
+		for (i = 0; i < sizeof(ch) / sizeof(char); i++)
+		{
+			if (*str == ch[i] || *str == ch[i] - 32)
+			{
+				*str = num[i] + 48;
+			}
+		}
+		str++;
+	}
+	return (ptr);
+}
