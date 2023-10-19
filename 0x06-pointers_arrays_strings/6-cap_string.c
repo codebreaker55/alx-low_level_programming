d85f98721ac4b8d77b3204b9d636e27620dea239
belal <belalismail252@gmail.com>
2023-10-19 02:54:26 -0700

    task Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,15 +18,16 @@ char *cap_string(char *str)
 
 			i++;
 
-		if (str[i - 1] == ' ' || str[i - 1] == '\t' ||
-		    str[i - 1] == '\n' || str[i - 1] == ',' ||
-		    str[i - 1] == ';' || str[i - 1] == '.' ||
-		    str[i - 1] == '!' || str[i - 1] == '?' ||
-		    str[i - 1] == '"' || str[i - 1] == '(' ||
-		    str[i - 1] == ')' || str[i - 1] == '{' ||
-		    str[i - 1] == '}')
-
+		if (str[i] == ' ' || str[i] == '\t' ||
+		    str[i] == '\n' || str[i] == ',' ||
+		    str[i] == ';' || str[i] == '.' ||
+		    str[i] == '!' || str[i] == '?' ||
+		    str[i] == '"' || str[i] == '(' ||
+		    str[i] == ')' || str[i] == '{' ||
+		    str[i] == '}')
+		{
 			str[i] -= 32;
+		}
 		i++;
 	}
 	return (str);
