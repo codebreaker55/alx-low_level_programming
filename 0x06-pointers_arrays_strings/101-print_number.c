ba49f36da6da9d6fa6e2e62421e65974ccedf98a
belal <belalismail252@gmail.com>
2023-10-19 10:25:09 -0700

    Numbers have life task

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints an integer
+ *
+ * @n: is the number of elements
+ *
+ * Return: 0
+*/
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+	_putchar(n % 10 + 48);
+}
