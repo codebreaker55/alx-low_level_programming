efc54470468f742485245a597c6e86c70f4b580c
belal <belalismail252@gmail.com>
2023-10-23 04:30:13 -0700

    the memcpy task

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,14 +13,11 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
-	char *tmp;
+	unsigned int cpy;
 
-	for (i = 0; i < n; i++)
+	for (cpy = 0; cpy < n; cpy++)
 	{
-		tmp[i] = src[i];
-		src[i] = dest[i];
-		dest[i] = tmp[i];
+		dest[cpy] = src[cpy];
 	}
 	return (dest);
 }
