36a25f995c1fb1f794e0fdc84b87520f71ce9e2f
belal <belalismail252@gmail.com>
2023-10-26 15:54:34 -0700

    before you bother telling me it's impossible task

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -11,7 +11,7 @@
 */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s2 == '*' || *s1 == *s2)
+	if (*s2 == '*' || (*s1 == *s2 && *s1 != '\0'))
 	{
 		return (wildcmp(s1 + 1, s2 + 1));
 	}
@@ -19,9 +19,9 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (1);
 	}
-	else if (*s1 == '*')
+	else if (*s2 == '*')
 	{
-		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2 + 1));
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	}
 	else
 	{
