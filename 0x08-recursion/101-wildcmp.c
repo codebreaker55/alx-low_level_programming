019605c986ed53a03bdf67e5d5c90916b30cb02c
belal <belalismail252@gmail.com>
2023-10-26 16:46:59 -0700

    impossible task

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -19,9 +19,9 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (1);
 	}
-	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+	if (*s2 == '*' && *(s2 + 1) == '\0' && *s1 == '\0')
 	{
-		return (0);
+		return (1);
 	}
 	if (*s2 == '*')
 	{
