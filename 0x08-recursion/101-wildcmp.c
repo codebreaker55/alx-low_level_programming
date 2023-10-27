082e4b7d5f5fe2ae8ea6ad82699e0fb65deaaa62
belal <belalismail252@gmail.com>
2023-10-26 17:38:29 -0700

    Task Inception

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -15,14 +15,16 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (wildcmp(s1 + 1, s2 + 1));
 	}
+
 	if (*s1 == '\0')
 	{
-		if (*s2 == '*' && *s2 != 0)
+		if (*s2 == '*' && *s2 != '\0')
 		{
 			return (wildcmp(s1, s2 + 1));
 		}
 		return (*s2 == '\0');
 	}
+
 	if (*s2 == '*')
 	{
 		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
