580a875d445d3901c74f96b19810406a13e541f6
belal <belalismail252@gmail.com>
2023-11-01 12:31:00 -0700

    show you how great I am task

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * wrdcnt - function that counts number of words
+ * wrdspr - used to count number of words
  *
- * @s: name of the string
+ * @s: name of the string to be counted
  *
  * Return: an integer
 */
 
-int wrdcnt(char *s)
+int wrdspr(char *s)
 {
 	int i, num = 0;
 
@@ -37,42 +37,44 @@ int wrdcnt(char *s)
 
 char **strtow(char *str)
 {
-	int i = 0, j, k, len = 0, num = 0, count = 0;
+	int i, j, k, len, num = 0, count = 0;
 	char **ptr;
 
 	if (str == NULL || *str == '\0')
 		return (NULL);
-	num = wrdcnt(str);
+	num = wrdspr(str);
 	if (num == 0)
 		return (NULL);
-	ptr = (char **) malloc((num + 1) * sizeof(char *));
+	ptr = (char **) malloc(num * sizeof(char *));
 	if (ptr == NULL)
 		return (NULL);
-	ptr[num] = NULL;
+	ptr[num - 1] = NULL;
+	i = 0;
 	while (str[i])
 	{
-	       if (str[i] != ' ' && (str[i - 1] == ' ' || i == 0))
-	       {
-		       for (j = 1; str[i + j] && str[i + j] != '\0'; j++)
-			       ;
-		       ptr[count] = (char *) malloc((j + 1) * sizeof(char));
-		       if (ptr[count] == NULL)
-		       {
-			       for (k = 0; k < count; k++)
-				       free(ptr[k]);
-			       free(ptr);
-			       return (NULL);
-		       }
-		       for (len = 0; len < j; len++)
-			       ptr[count][len] = str[i + len];
-		       ptr[count][len] = '\0';
-		       count++;
-		       i = i + j;
-	       }
-	       else
-	       {
-		       i++;
-	       }
+		if (str[i] != ' ' && (str[i - 1] == ' ' || i == 0))
+		{
+			for (j = 1; str[i + j] && str[i + j] != ' '; j++)
+				;
+			j++;
+			ptr[count] = (char *) malloc(j * sizeof(char));
+			j--;
+			if (ptr[count] == NULL)
+			{
+				for (k = 0; k < count; k++)
+					free(ptr[k]);
+				free(ptr[num - 1]);
+				free(ptr);
+				return (NULL);
+			}
+			for (len = 0; len < j; len++)
+				ptr[count][len] = str[i + len];
+			ptr[count][len] = '\0';
+			count++;
+			i = i + j;
+		}
+		else
+			i++;
 	}
 	return (ptr);
 }
