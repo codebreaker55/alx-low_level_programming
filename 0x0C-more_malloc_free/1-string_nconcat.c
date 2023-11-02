7be5cfe13ce9c99bbd0b1e2fd9121346b212a8e2
belal <belalismail252@gmail.com>
2023-11-02 06:38:44 -0700

    String_nconcat task

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,9 +33,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; i < len1; i++)
 		ptr[i] = s1[i];
-	i++;
+
 	for (j = 0; j < n; j++)
 	{
 		ptr[i] = s2[j];
