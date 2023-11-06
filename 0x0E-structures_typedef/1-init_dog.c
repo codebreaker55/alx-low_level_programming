c7647ba3bb1716da4c099d4e22278fadb3a3ca8e
belal <belalismail252@gmail.com>
2023-11-06 08:54:57 -0800

    dog is the only thing on earth task

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog - function that initialize a variable of type struct dog
+ *
+ * @d: is the variable to be initialized
+ * @name: is the name of the dog
+ * @age: is the age of the dog
+ * @owner: is the name of the owner of the dog
+ *
+ * Return: nothing
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	(*d).age = age;
+	d->owner = owner;
+}
