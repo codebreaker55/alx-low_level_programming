ee532a7f4115dd95589e86ce5bb97e71b9b16a4a
belal <belalismail252@gmail.com>
2023-11-06 08:23:13 -0800

    Poppy task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - is used to create variables 
+ *	that hold information about individual dogs
+ *
+ * @name: is the name of the dog
+ * @age: is the age of the dog
+ * @owner: is the name of the dog owner
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
