62966c036edbc28f432ec134cebb09547bf26083
belal <belalismail252@gmail.com>
2023-11-06 12:27:48 -0800

    How many legs does a dog have task

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - function that frees dogs
+ *
+ * @d: is the name of a pointer
+ *
+ * Return: nothing
+*/
+
+void free_dog(dog_t *d)
+{
+	if (d != 0)
+	{
+		if (d->name != 0)
+		{
+			free(d->name);
+		}
+		else if ((*d).owner != 0)
+		{
+			free((*d).owner);
+		}
+		free(d);
+	}
+}
