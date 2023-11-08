6b7eb7d63f024fa725d444f107e93c2e05cd9c4e
belal <belalismail252@gmail.com>
2023-11-08 15:07:36 -0800

    Always meant to be task

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -24,9 +24,13 @@ int (*get_op_func(char *s))(int, int)
 
 	int i = 0;
 
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	while (i < 5)
 	{
+		if (s != 0 && s[0] == ops[i].op[0] && !s[1])
+		{
+			return (ops[i].f);
+		}
 		i++;
 	}
-	return (ops[i].f);
+	return (NULL);
 }
