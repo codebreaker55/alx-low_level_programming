6b3209d350c17bf00ea7b28cd457697b65dd1aee
belal <belalismail252@gmail.com>
2023-11-08 09:57:38 -0800

    serves simply as something to aim at task

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,39 @@
+#include "3-calc.h"
+
+/**
+ * main - is a program that performs simple operations
+ *
+ * @argc: is the number of arguments
+ * @argv: is the argument vector
+ *
+ * Return: always 0 (Success)
+*/
+
+int main(int argc, char **argv)
+{
+	int (*opr_fun)(int, int);
+	int i, j;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	i = atoi(argv[1]);
+	j = atoi(argv[3]);
+	opr_fun = get_op_func(argv[2]);
+
+	if (opr_fun == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (!j && (argv[2][0] == '/' || argv[2][0] == '%'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", opr_fun(i, j));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,72 @@
+#include "3-calc.h"
+
+/**
+ * op_add - function used to add two integers
+ *
+ * @a: is an integer
+ * @b: is an integer
+ *
+ * Return: the sum of a and b
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function used to subtruct two integers
+ *
+ * @a: is an integer
+ * @b: is an integer
+ *
+ * Return: the difference of a and b
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - function used to multiply two integers
+ *
+ * @a: is an integer
+ * @b: is an integer
+ *
+ * Return: the product of a and b
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - function used to divide two integers
+ *
+ * @a: is an integer
+ * @b: is an integer
+ *
+ * Return: the result of the division of a by b
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - function used to get the remainder of division
+ *	of two integers
+ *
+ * @a: is an integer
+ * @b: is an integer
+ *
+ * Return: the remainder of the division of a by b
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
