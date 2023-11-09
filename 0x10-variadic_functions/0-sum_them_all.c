797afeceed54a3828b0fcfbfc78d12fd13ed6056
belal <belalismail252@gmail.com>
2023-11-09 07:13:45 -0800

    Beauty is variable task

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - function that returns the sum of all its parameters
+ *
+ * @n: is a constant unsigned integer
+ *
+ * Return: the sum of all its parameters
+ *	if n equal zero return 0
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	int res, num;
+
+	va_start(args, n);
+	res = 0;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	for (num = 0; num <=  n; num++)
+	{
+		res += va_arg(args, int);
+	}
+	va_end(args);
+	return (res);
+}
