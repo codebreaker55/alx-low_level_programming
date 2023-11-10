787bf9cf9ada0276c85a9d58d719a6b56db7d025
belal <belalismail252@gmail.com>
2023-11-10 07:25:23 -0800

    new attemp

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -26,11 +26,11 @@ int (*get_op_func(char *s))(int, int)
 
 	while (i < 5)
 	{
-		if (s != 0 && s[0] == ops[i].op[0] && !s[1])
+		if (s[0] == ops->op[i])
 		{
-			return (ops[i].f);
+			break;
 		}
 		i++;
 	}
-	return (NULL);
+	return (ops[i / 2].f);
 }
