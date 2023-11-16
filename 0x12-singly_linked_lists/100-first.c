e5b9d166a20156ad2739e4ab145594a8c2d43a7b
belal <belalismail252@gmail.com>
2023-11-16 10:40:46 -0800

    The Hare and the Tortoise task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * _before_main - a function that executes before the main function is executed
+ *
+ * Return: nothing
+*/
+
+void _before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
