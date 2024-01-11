5f789c190ae5a2b7aa399d898ec55e62f9a5957e
belal <belalismail252@gmail.com>
2024-01-11 04:09:59 -0800

    List length task

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -4,7 +4,7 @@
  * dlistint_len - function that returns the number of elements
  *	in a linked dlistint_t list
  *
- * @h is the address of the head
+ * @h: is the address of the head
  *
  * Return: the number of elements
 */
