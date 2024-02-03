c770cf8d4d9955247d7677e10747dcd08157f990
belal <belalismail252@gmail.com>
2024-02-02 16:51:10 -0800

    hash delete task

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - function that deletes a hash table
+ *
+ * @ht: is the hash table
+ *
+ * Return: nothing
+*/
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int n;
+	hash_node_t *n_node;
+
+	if (ht == NULL || ht->size == 0 || ht->array == NULL)
+	{
+		return;
+	}
+	for (n = 0; n < ht->size; n++)
+	{
+		while (ht->array[n] != NULL)
+		{
+			n_node = ht->array[n]->next;
+			free(ht->array[n]->key);
+			free(ht->array[n]->value);
+			free(ht->array[n]);
+			ht->array[n] = n_next;
+		}
+	}
+	free(ht->array);
+	ht->array = NULL;
+	ht->size = 0;
+	free(ht);
+}
